ffcdc627f4c01fb9d22f365c18a966f109968f82
Hannah Joy Hotricano <hannahotricano@gmail.com>
2025-10-26 18:08:48 +0800

    .

--- Database/employee_management_system_db.sql
+++ Database/employee_management_system_db.sql
@@ -39,10 +39,7 @@ CREATE TABLE `tbl_admin` (
 --
 
 INSERT INTO `tbl_admin` (`admin_ID`, `name`, `username`, `password`) VALUES
-(4, 'Hannah', 'hanna@gmail.com', '$2y$10$BWEWB54rsyKvzBv/kWOmgujq2712VVUXDxOAn.I2AJcdr1QxzJyvC'),
-(5, 'Ella Hotricano', 'ella05', '$2y$10$NhEhSfx0RV5hYYJ/KheehuvX9qEU5t9qX.bhGYY3mAT8AnybjO5/O'),
-(6, 'Faith', 'faith@gmail.com', '$2y$10$T.l417lPVDTTIVlxzXSnTOyezUw9.lKQnwc14tEFTTZbVFM7lZJ9e'),
-(8, 'Hannah Joy', 'joy@gmail.com', '$2y$10$dstq6KSmMh8GYbmmzaq15.vhQa4tKeoY9IzlLVfvRsAVsKUf48Rye');
+(1, 'Admin', 'admin123@gmail.com', 'admin123'),
 
 -- --------------------------------------------------------
 
@@ -58,14 +55,6 @@ CREATE TABLE `tbl_bank_info` (
   `emp_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tbl_bank_info`
---
-
-INSERT INTO `tbl_bank_info` (`bank_ID`, `acc_name`, `acc_no`, `bank_name`, `emp_ID`) VALUES
-(1, 'Faith Hotricano', '54-0909-7676', 'Land Bank', 5),
-(2, 'Audrey Ignacio', '6766-90900-899', 'Security Bank', 6),
-(3, 'Claire Hotricano', '688-99-657-876', 'Security Bank', 7);
 
 -- --------------------------------------------------------
 
@@ -96,14 +85,6 @@ CREATE TABLE `tbl_employee_account` (
   `rate` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tbl_employee_account`
---
-
-INSERT INTO `tbl_employee_account` (`emp_acc_ID`, `last_name`, `first_name`, `middle_name`, `suffix_name`, `position`, `standing`, `province_address`, `municipal_address`, `barangay_address`, `street_address`, `block_no`, `phone_no`, `birth_date`, `age`, `gender`, `email`, `username`, `password`, `rate`) VALUES
-(5, 'Hotricano', 'Faith', 'Ompong', 'N/A', 'Human Resource', 'Regular', 'Biliran', 'Naval', 'P.I. Garcia', 'Caneja Ext.', '1223', '09090990909', '2010-09-06', '15', 'Female', 'faith01@gmail.com', 'faith01', '$2y$10$9Ga0uKBdvXhBYceW8zbDvejyb1hmBnCADU8ZtbGMbnVuS1o0sgb4y', 600),
-(6, 'Ignacio', 'Audrey Rose', 'Patiga', 'N/A', 'Manager', 'Regular', 'Biliran', 'Naval', 'P.I. Garcia', 'Vicentillo   ', '76755', '0987654321', '2003-01-28', '21', 'Female', 'audrey@gmail.com', 'odrey@gmail.com', '$2y$10$sfVyWaGZiaYHTJ348uGODuBcmKNIJ5YOj3e1iERVO6cnVuoQRZpPa', 1500),
-(7, 'Hotricano', 'Claire', 'Ompong', 'N/A', 'Administrator', 'Regular', 'Biliran', 'Naval', 'P.I. Garcia', 'Caneja Ext.', '873864', '8238648364', '2001-03-03', '23', 'Female', 'claire@yahoo.com', 'claire@gmail.com', '$2y$10$fiwdS5GqQ8DPOEs9PZFNUuGvh6GnoPKogf4LaiduGcLj6lyB.6Iqu', 1000);
 
 -- --------------------------------------------------------
 
@@ -119,14 +100,7 @@ CREATE TABLE `tbl_hr` (
   `task` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tbl_hr`
---
 
-INSERT INTO `tbl_hr` (`HR_ID`, `name`, `username`, `password`, `task`) VALUES
-(2, 'Jennie Kim', 'jennie02', '$2y$10$WqxsiQvwt0rO9Q.MU4mSfu.GmWqmNiaQxbq4oG5Rbzh0egUceeqh.', 'Recruitment'),
-(3, 'Hannah Joy', 'hannah03', '$2y$10$ifJb/28/4zo6xSpzvwxUPuIDNw5D0J6K087mmXIov6CZzpH1l0mSG', 'Payroll'),
-(4, 'Audrey Rose', 'audrey@gmail.com', '$2y$10$szvP7VI61ZWJ8QV.bAQKsu.j0eRWihuUytmtH7IKuqCawVkrJUwFO', 'Payroll');
 
 -- --------------------------------------------------------
 
@@ -142,14 +116,7 @@ CREATE TABLE `tbl_manager` (
   `dept` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tbl_manager`
---
 
-INSERT INTO `tbl_manager` (`manager_ID`, `name`, `username`, `password`, `dept`) VALUES
-(2, 'Drake Carcellar', 'drake01', '$2y$10$pDyY4wovTl82O6rZ4.cl7OB2ug/Jj.9gkhf6vIWXARVnPn0krlW6C', 'Security'),
-(3, 'Audrey Marie', 'audreng@gmail.com', '$2y$10$WPdDmOyy2lwm.RoqKhV3nOgKV/9m990ztpyWDDK/Ndd47.pVU87M.', 'Kitchen'),
-(4, 'Hannah Hotricano', 'hannah04', '$2y$10$Dme85G.murvtNtUDudsoeORa6ZeQYTTXRtkU52Gp90Wfijtkw5OTO', 'Front office');
 
 -- --------------------------------------------------------
 
@@ -172,19 +139,7 @@ CREATE TABLE `tbl_salary` (
   `emp_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tbl_salary`
---
 
-INSERT INTO `tbl_salary` (`salary_ID`, `monthly_rate`, `overtime_rate`, `philhealth`, `SSS`, `pagibig`, `TIN_ID`, `penalties`, `total_deduction`, `total_salary`, `date_given`, `emp_ID`) VALUES
-(1, 7000, 100, 100, 100, 100, 100, 50, 450, 6650, '2024-05-12 19:39:11', 7),
-(2, 8000, 100, 100, 100, 100, 100, 100, 500, 7600, '2024-05-12 19:43:01', 5),
-(3, 8000, 100, 100, 100, 100, 100, 100, 500, 7600, '2024-05-12 19:45:59', 6),
-(4, 8000, 100, 100, 100, 100, 100, 0, 400, 7700, '2024-05-12 20:02:26', 5),
-(5, 8000, 100, 100, 100, 100, 100, 0, 400, 7700, '2024-05-12 07:00:00', 5),
-(6, 8000, 0, 100, 100, 100, 100, 0, 400, 7600, '2024-05-12 20:38:33', 5),
-(7, 8000, 0, 100, 100, 100, 100, 0, 400, 7600, '2024-05-13 00:37:00', 7),
-(8, 8000, 100, 100, 100, 100, 100, 100, 500, 7600, '2024-05-13 00:44:33', 7);
 
 -- --------------------------------------------------------
 
@@ -205,24 +160,7 @@ CREATE TABLE `tbl_time_inout_record` (
   `emp_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tbl_time_inout_record`
---
 
-INSERT INTO `tbl_time_inout_record` (`InOut_ID`, `attend_date`, `attend_status`, `time_in`, `in_status`, `time_out`, `out_status`, `late_penalty`, `ot_rate`, `emp_ID`) VALUES
-(1, '2024-05-10', 'Absent', '07:30:00', '-', '05:00:00', '-', 0, 0, 6),
-(2, '2024-05-10', 'Duty', '08:30:00', 'Late', '18:00:00', 'Over Time', 50, 100, 5),
-(3, '2024-05-10', 'Duty', '07:30:00', 'On Time', '17:00:00', 'On Time', 0, 0, 7),
-(4, '2024-05-11', 'Duty', '07:30:00', 'On Time', '19:00:00', 'Over Time', 0, 100, 5),
-(5, '2024-05-11', 'Duty', '09:30:00', 'Late', '05:00:00', 'On Time', 50, 0, 7),
-(6, '2024-05-11', 'Duty', '7:30 AM', 'On Time', '5:00 PM', 'On Time', 0, 0, 5),
-(7, '2024-05-12', 'Duty', '8:00 AM', 'Late', '7:00 PM', 'Over Time', 50, 200, 5),
-(8, '2024-05-12', 'Duty', '7:30 AM', 'On Time', '5:00 PM', 'On Time', 0, 0, 5),
-(9, '2024-05-12', 'Duty', '7:30 AM', 'On Time', '5:00 PM', 'On Time', 0, 0, 7),
-(10, '2024-05-12', 'Duty', '7:30 AM', 'On Time', '5:00 PM', 'On Time', 0, 0, 6),
-(11, '2024-05-12', 'Absent', '-', '-', '-', '-', 0, 0, 6),
-(12, '2024-05-13', 'Absent', '-', '-', '-', '-', 0, 0, 7),
-(13, '2025-02-15', 'Duty', '8:00 AM', 'On Time', '9:00 PM', 'Over Time', 0, 100, 5);
 
 --
 -- Indexes for dumped tables
